37bbe27ff4cc499291350820cdf7678cb2db8128
Vanessa Martinez <vanss472@gmail.com>
2021-03-10 11:26:22 -0500

    change a tag to button for accessibility

--- src/components/nav-bar/index.js
+++ src/components/nav-bar/index.js
@@ -33,7 +33,7 @@ export default function NavBar() {
            data-testid="navigation-tabs">
         {
           data.map((item) =>
-            <a key={item.id} onClick={() => setActive(item)} className={active === item ? 'active' : ''}>{item.tabName}</a>
+            <button key={item.id} onClick={() => setActive(item)} className={active === item ? 'active' : ''}>{item.tabName}</button>
           )
         }
       </div>
